c8597e59d6ac0318421438df99bead95cf2ca893
kleinfelter <github.com@kevin.kleinfelter.com>
2017-11-14 17:08:50 -0500

    unknown

--- runme-local.sh
+++ runme-local.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-bundle exec jekyll serve --incremental
+source /Users/kevin/.bash_profile
+cd /Users/kevin/Sync/Sites/k4kpk.github.io
+chruby 2.4.2
+bundle exec jekyll serve --incremental --watch
