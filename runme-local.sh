ecc60b282fafc4d98979ceb194e4641f8f07798e
kleinfelter <github.com@kevin.kleinfelter.com>
2017-11-14 18:05:25 -0500

    msg

--- runme-local.sh
+++ runme-local.sh
@@ -2,4 +2,4 @@
 source /Users/kevin/.bash_profile
 cd /Users/kevin/Sync/Sites/k4kpk.github.io
 chruby 2.4.2
-bundle exec jekyll serve --incremental --watch
+bundle exec jekyll serve --watch --incremental
