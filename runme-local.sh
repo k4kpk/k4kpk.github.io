31c046d860c35f8c443119264e4898f675dc547f
kevin <kevin@boxtop.kleinfelter.org>
2019-02-26 16:43:56 -0500

    message

--- runme-local.sh
+++ runme-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash  
-rm /tmp/k4kpk.jekyll.???
-rm /tmp/k4kpk.github.io.???
-cd /Users/kevin/Sync/Sites/k4kpk.github.io  
+rm /tmp/k4kpk.jekyll.??? 2> /dev/null
+rm /tmp/k4kpk.github.io.??? 2> /dev/null
+cd /home/kevin/sites/k4kpk.github.io  
 # Need to give it a few seconds to allow docker daemon to start
 sleep 5
 /usr/local/bin/docker-compose up > /tmp/k4kpk.github.io.out 2>/tmp/k4kpk.github.io.err
